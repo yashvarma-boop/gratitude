eda03271f3cb816d738ea8fb77a1dd1abe9a1fd9
Yashybaby! <yash.varma@empresscapital.vc>
2026-02-01 23:27:10 +1100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,13 +10,27 @@
 // 6. Enable Cloud Firestore (start in test mode, then add rules)
 // =============================================
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDumWfmbSqwGbAmzCpUjTLVU6qic8Hpu1Q",
+  authDomain: "flourishly-67bd1.firebaseapp.com",
+  projectId: "flourishly-67bd1",
+  storageBucket: "flourishly-67bd1.firebasestorage.app",
+  messagingSenderId: "861477808589",
+  appId: "1:861477808589:web:adf3a1abdd5896e6b47c84",
+  measurementId: "G-179V9WEP3P"
 };
 
 // Initialize Firebase
